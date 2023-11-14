a1c023f29219bdc723cb27600463c5ef2dadd2f3
Graciela de la Cruz <graciela_cruz@live.com.mx>
2023-11-14 06:14:47 -0600

    first commit

--- src/templates/layout.jsx
+++ src/templates/layout.jsx
@@ -8,16 +8,16 @@ function Layout({children}) {
             <Grid container sx={{flexGrow: 1}}>
               <Grid item flexBasis={220}>
                  Menu goes here 
-                 </Grid>
-                 <Grid item flex={1}>
+              </Grid>
+              <Grid item flex={1}>
                 <Grid xs={12}>HEADER</Grid>
                 <Grid container spacing={3}> 
                 <Grid xs={3}>PKI</Grid>
                 <Grid xs={3}>PKI</Grid>
                 <Grid xs={3}>PKI</Grid>
                 <Grid xs={3}>PKI</Grid>
-                </Grid>
-                <Grid container spacing={3}>
+              </Grid>
+              <Grid container spacing={3}>
                 <Grid container xs={8}>Container 1</Grid>
                 <Grid container xs={4}>Container 2</Grid>
                 </Grid>
